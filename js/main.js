bf818eec72e445b40060b1b7b6cd66e99badded9
Andy Ball <932400399@bun2-206-11.local>
2019-08-02 11:47:28 +1200

    logging weather. Ajax rq inside Ajax rq to protect key

--- js/main.js
+++ js/main.js
@@ -1,4 +1,40 @@
 $(document).ready(function(){
-  console.log('hello world');
+  // console.log('hello world');
+
+  // config file request
+  let darkSkyKey;
+
+  $.ajax({
+    url: 'config.json',
+    type: 'GET',
+    dataType: 'json',
+    success: function(keys){
+      darkSkyKey = keys['darkSkyKey'];
+      getWeatherData();
+    },
+    error: function(){
+      console.log('can\'t find config.json');
+    }
+  });
+
+  // API request
+  getWeatherData = () => {
+    $.ajax({
+      url: `https://api.darksky.net/forecast/${darkSkyKey}/-41.2865,174.7762`,
+      type: 'GET',
+      dataType: 'jsonp',
+      success: function(data){
+        console.log(data);
+      },
+      error: function() {
+        console.log('something bad happened');
+      }
+    })
+  }
+
+  // let's use the google places autocomplete API!
+  // let's maybe just grab the code off the class github repo ?
+  // let's use it to pass co-ordinates into the ajax request we're sending to dark-sky
+
 
 });
